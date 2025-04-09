2e940a7090e43f8690847bf409fca39874ff514e
zitongcharliedeng <108423881+zitongcharliedeng@users.noreply.github.com>
2025-04-09 20:58:01 +0000

    feat: apply styles programmatically (using test default of binary buttons)

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -3,6 +3,30 @@ import '../style.css';
 import '../App.css';
 
 async function onActivate(plugin: ReactRNPlugin) {
+  function numberOfAnswerButtons() {
+    return 2;
+  };
+
+  plugin.app.registerCSS(
+    'queue-container',
+    `
+     .rn-queue__answer-btn--immediately {
+        display: none;
+      }
+      
+      .rn-queue__answer-btn--partial {
+        display: none;
+      }
+      
+      .rn-queue__answer-btn--too-soon {
+        display: none;
+      }
+      
+      .spaced-repetition__accuracy-buttons {
+        grid-template-columns: repeat(${numberOfAnswerButtons()}, minmax(0, 1fr)) !important;
+      }
+    `
+  );
 }
 
 async function onDeactivate(_: ReactRNPlugin) {}
