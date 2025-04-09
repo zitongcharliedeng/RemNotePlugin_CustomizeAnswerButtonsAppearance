2d68bb1008121e3aa8a9f4d3f57ccb2296aa9bf8
zitongcharliedeng <108423881+zitongcharliedeng@users.noreply.github.com>
2025-04-09 21:07:05 +0000

    refactor: grab style state from helper function

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -7,21 +7,46 @@ async function onActivate(plugin: ReactRNPlugin) {
     return 2;
   };
 
+  function getSavedDisplayStyleForButton(button: string) {
+    switch (button) {
+      case 'immediately':
+        return 'none';
+      case 'with-effort':
+        return 'inherit';
+      case 'partial':
+        return 'none';
+      case 'forgotten':
+        return 'none';
+      case 'too-soon':
+        return 'inherit';
+      default:
+        return 'inherit';
+    }
+  }
+
   plugin.app.registerCSS(
     'queue-container',
     `
-     .rn-queue__answer-btn--immediately {
-        display: none;
+      .rn-queue__answer-btn--immediately {
+        display: ${getSavedDisplayStyleForButton('immediately')};
+      }
+
+      .rn-queue__answer-btn--with-effort {
+        display: ${getSavedDisplayStyleForButton('with-effort')};
       }
       
       .rn-queue__answer-btn--partial {
-        display: none;
+        display: ${getSavedDisplayStyleForButton('partial')};
       }
       
+      .rn-queue__answer-btn--forgotten {
+        display: ${getSavedDisplayStyleForButton('forgotten')};
+      }
+
       .rn-queue__answer-btn--too-soon {
-        display: none;
+        display: ${getSavedDisplayStyleForButton('too-soon')};
       }
-      
+
       .spaced-repetition__accuracy-buttons {
         grid-template-columns: repeat(${numberOfAnswerButtons()}, minmax(0, 1fr)) !important;
       }
